08c6331ea6e2ab9dabad30d7c5b03b5f512984b4
Marco Navarro <7016419+marconn@users.noreply.github.com>
2020-12-21 16:52:55 -0600

    Initial commit

---
+++ src/AppOptionsApi.vue
@@ -0,0 +1,74 @@
+<template>
+    <section class="container">
+        <user-data
+            class="test"
+            :first-name="firstName"
+            :last-name="lastName"
+            :age="age"
+        ></user-data>
+        <button @click="setAge">Change Age</button>
+        <div>
+            <input type="text" placeholder="First Name" v-model="firstName" />
+            <input type="text" placeholder="Last Name" ref="lastNameInput" />
+            <button @click="setLastName">Set Last Name</button>
+        </div>
+    </section>
+</template>
+
+<script>
+// import { ref, computed, watch, provide } from 'vue';
+import UserData from './components/UserData.vue';
+
+export default {
+    components: {
+        UserData,
+    },
+    data() {
+        return {
+            firstName: 'Roland',
+            lastName: 'Cambridge',
+            age: 31,
+        };
+    },
+    methods: {
+        setNewAge() {
+            this.age = 32;
+        },
+        setLastName() {
+          this.lastName = this.$refs['lastNameInput']
+          this.$set(this.adress, 'Llano los Angeles');
+        }
+    },
+    watch: {
+        age(val) {
+            console.log(val);
+        },
+    },
+    provide() {
+        return { userAge: this.age };
+    },
+};
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
+}
+
+.container {
+    margin: 3rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+    text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import App2 from './AppOptionsApi.vue';
+
+createApp(App).mount('#app');
+createApp(App2).mount('#app2');
